7736e59e0729a59120aa50a4a1698b2634654f6d
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-23 02:06:14 -0500

    Uploading changes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [addCart, setAddCart] = useState(true)
+
+  const shop = addCart ? 'in-cart' : ""
+
+  const buttonText = shop ? 'Remove From Cart' : "Add to Cart"
+
+  function handleClick() {
+    setAddCart(addCart => !addCart)
+  }
+
   return (
-    <li className="">
+    <li className={shop}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={shop}>{buttonText}</button>
     </li>
   );
 }
